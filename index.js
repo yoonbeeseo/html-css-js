d493f323abe9d5f8f5830665a1af200ee13b19f3
fckillernaver <fckiller@naver.com>
2025-02-17 17:05:25 +0900

    main init

--- index.js
+++
@@ -1,7 +0,0 @@
-const imgs = [
-  "https://cdn-icons-png.flaticon.com/128/9534/9534501.png",
-  "https://cdn-icons-png.flaticon.com/128/3562/3562093.png",
-  "https://cdn-icons-png.flaticon.com/128/12355/12355903.png",
-]
-
-console.log(imgs)
